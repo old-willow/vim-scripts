5e135653e5505409bca0cb52a184db740f6cc77a
old-willow <robert.kolozsi@gmail.com>
2019-05-14 23:29:13 +0200

    Adding vim-configure.sh file

---
+++ vim-configure.sh
@@ -0,0 +1,16 @@
+## This is just an example.
+
+./configure --prefix=/home/robi/opt/vim \
+--enable-pythoninterp=yes \
+--enable-python3interp=yes \
+--enable-multibyte \
+--enable-fontset \
+--enable-gui=gtk2 \
+--with-features=huge \
+--with-x \
+--enable-gtk2-check \
+--enable-gnome-check \
+--with-compiledby="robert" \
+--enable-cscope \
+--with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ \
+--with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu/
